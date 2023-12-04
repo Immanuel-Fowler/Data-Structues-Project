f65c4c4ca15000659180317780582a126dc1205d
Manny <60634841+Immanuel-Fowler@users.noreply.github.com>
2023-12-04 18:32:53 -0500

    Create GameLinkedList.py

---
+++ GameLinkedList.py
@@ -0,0 +1,67 @@
+import os
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.node_count = 0
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node  # Update tail for the first node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+            new_node.prev = current
+            self.tail = new_node
+
+        self.node_count +=1
+    def prepend(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+        self.node_count +=1
+
+    def delete(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                if current.prev:
+                    current.prev.next = current.next
+                else:
+                    self.head = current.next
+                if current.next:
+                    current.next.prev = current.prev
+                return
+            current = current.next
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+Games = DoublyLinkedList()
+
+directory = os.getcwd()+'/Games'
+
+for item in os.listdir(directory):
+    item_path = os.path.join(directory, item)
+    if os.path.isdir(item_path):
+        Games.append([item_path.split("/")[len(item_path.split("/"))-1], item_path + "/thumbnail.png", item_path + "/Main.py"])
